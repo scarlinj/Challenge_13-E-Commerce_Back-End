52e6be8b888f77a931cf77abd14359961f3205b4
Stephen Carlin <scarlinj@gmail.com>
2022-06-20 21:17:42 -0700

    fixed npm start

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const userRoutes = require('./user-routes.js');
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
+router.use('/users', userRoutes);
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
